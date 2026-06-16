80cc090a4f0fd56428312fb80a5a3eba8f8a768d
spsnft <87615686+spsnft@users.noreply.github.com>
2026-06-16 22:51:39 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      // Явно принуждаем компилятор использовать стандарты Svelte 5 Runes
+      compilerOptions: {
+        runes: true
+      }
+    })
+  ]
 });
