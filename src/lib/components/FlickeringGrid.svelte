401ebbc55ac158520808d7e83ed9248742cba9a9
spsnft <87615686+spsnft@users.noreply.github.com>
2026-06-17 12:02:03 +0700

    Update FlickeringGrid.svelte

--- src/lib/components/FlickeringGrid.svelte
+++ src/lib/components/FlickeringGrid.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-	import { cn } from "$lib/utils";
+	import { cn } from "../utils.js";
 
 	interface FlickeringGridProps {
 		squareSize?: number;
