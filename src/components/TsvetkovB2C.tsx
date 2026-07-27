d09517e18c8aba7825c0ff613d9771719172c51a
spsnft <87615686+spsnft@users.noreply.github.com>
2026-07-27 21:44:53 +0700

    Update TsvetkovB2C.tsx

--- src/components/TsvetkovB2C.tsx
+++ src/components/TsvetkovB2C.tsx
@@ -34,7 +34,7 @@ export default function TsvetkovB2C({ lang }: TsvetkovB2CProps) {
         <Bottleneck lang={lang} />
         <Expertise dict={dict} />
         <Services dict={dict} />
-        <CaseStudies />
+        <CaseStudies dict={dict} />
         <Contact dict={dict} />
       </div>
     </main>
