5f5687f4ab58902dd811e58fedb0cfc001cf9c00
spsnft <87615686+spsnft@users.noreply.github.com>
2026-07-27 19:55:19 +0700

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,6 +1,11 @@
 import TsvetkovB2C from '@/src/components/TsvetkovB2C';
 
-export default function Home({ params: { lang } }: { params: { lang: string } }) {
-  // Передаем язык вниз в компоненты, чтобы они могли подтянуть нужный JSON
+type PageProps = {
+  params: Promise<{ lang: string }>;
+};
+
+export default async function Home({ params }: PageProps) {
+  const { lang } = await params;
+  
   return <TsvetkovB2C lang={lang} />;
 }
